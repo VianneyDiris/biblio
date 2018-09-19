38525659f770bf702b2d3d862ee79b02f3f1a683
vianney diris <vianney.diris@gmail.com>
2018-09-19 21:07:59 +0200

    ajout de l'envoie de mail via api javamail pour Gmail

---
+++ scheduler/src/main/java/com/batch/app/mail/Report.java
@@ -0,0 +1,61 @@
+package com.batch.app.mail;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Report {
+	@Value("${batch.mail}") 
+	private String mail;
+	
+	@Value("${batch.password}") 
+	private String password;
+	
+	
+    public void sendMail() {
+        //Setting up configurations for the email connection to the Google SMTP server using TLS
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        //Establishing a session with required user details
+        System.out.println(mail+password);
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(mail, password);
+            }
+        });
+        try {
+            //Creating a Message object to set the email content
+            MimeMessage msg = new MimeMessage(session);
+            //Storing the comma seperated values to email addresses
+            String to = "koyuko59500@gmail.com,h26apassif@gmail.com";
+            /*Parsing the String with defualt delimiter as a comma by marking the boolean as true and storing the email
+            addresses in an array of InternetAddress objects*/
+            InternetAddress[] address = InternetAddress.parse(to, true);
+            //Setting the recepients from the address variable
+            msg.setRecipients(Message.RecipientType.TO, address);
+            String timeStamp = new SimpleDateFormat("yyyymmdd_hh-mm-ss").format(new Date());
+            msg.setSubject("Fin de location");
+            msg.setSentDate(new Date());
+            msg.setText("Merci de bien vouloir nous retourner les ouvrages suivants");
+            msg.setHeader("XPriority", "1");
+            Transport.send(msg);
+            System.out.println("Mail has been sent successfully");
+        } catch (MessagingException mex) {
+            System.out.println("Unable to send an email" + mex);
+        }
+    }
+}
--- scheduler/src/main/java/com/batch/app/scheduler/BatchTasklet.java
+++ scheduler/src/main/java/com/batch/app/scheduler/BatchTasklet.java
@@ -4,16 +4,23 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import com.batch.app.mail.Report;
 
 @Component
 public class BatchTasklet implements Tasklet {
  
   @Value("${batch.message}") private String message;
+  
+  @Autowired
+  private Report report;
  
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
     System.out.println(message);
+    report.sendMail();
     return RepeatStatus.FINISHED;
   }
 
